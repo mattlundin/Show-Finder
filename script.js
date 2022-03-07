86b474b29a9db7ff52cf464b6ac146f2eb2d2d87
mattlundin <74123405+mattlundin@users.noreply.github.com>
2022-03-07 11:03:03 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('#searchForm');
+const container = document.querySelector('.container');
+
+form.addEventListener('submit', async function (e) {
+	e.preventDefault();
+	const searchTerm = form.elements.query.value;
+	const config = { params: { q: searchTerm } };
+	const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+	showImage(res.data);
+
+	form.elements.query.value = '';
+});
+
+const showImage = (shows) => {
+	for (let result of shows) {
+		if (result.show.image) {
+			const img = document.createElement('img');
+			img.src = result.show.image.medium;
+			container.append(img);
+		}
+	}
+};
